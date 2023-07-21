b4410df3a189618794dcab7b06cc424855a96aaa
rastylus <ryan.p.riggs@gmail.com>
2023-07-21 06:54:42 -0500

    added preventDefault

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 // Write your JavaScript code here!
+// window.addEventListener("load", function() {
+// }
+
+
 
 window.addEventListener("load", function() {
+    // console.log(validateInput(6));
 
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        console.log("test")
+        // alert("test");
+        event.preventDefault();
+        // formSubmission(document, list, pilot.value, copilot.value, fuelLevel.value, cargoLevel.value)
+    });
+    // let list = document.getElementById("faultyItems");
+    // // list.style.visibility = "hidden";
+    // let pilot = document.querySelector("input[name=pilotName");
+    // let copilot = document.querySelector("input[name=copilotName");
+    // let fuelLevel = document.querySelector("input[name=fuelLevel");
+    // let cargoLevel = document.querySelector("input[name=cargoMass");
+
+    // let pilotStatus = document.getElementById("pilotStatus");
+    // let copilotStatus = document.getElementById("copilotStatus");
+    // let fuelStatus = document.getElementById("fuelStatus");
+    // let cargoStatus = document.getElementById("cargoStatus");
+    // let launchStatus = document.getElementById("launchStatus");
+    
+    
+    // console.log(list);
+    
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
@@ -12,5 +40,15 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
+
    
+});
+
+window.addEventListener("load", function() {
+
+//     let form = document.querySelector("form");
+//     form.addEventListener("submit", function(event) {
+//         // alert("test");
+//         event.preventDefault();
+//     });
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -16,11 +16,48 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
    */
 }
 
-function validateInput(testInput) {
-   
-}
+// function validateInput(testInput) {
+//     let numInput = Number(testInput);
+//     if (testInput === "") {
+//         return "Empty";
+//     } else if (isNaN(numInput)) {
+//         return "Not a Number";
+//     } else if (numInput) {
+//         return "Is a Number";
+//     }
+// }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+// function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+//     // let items = document.getElementById("faultyItems");
+//     // console.log(document.getElementById("faultyItems"))
+//     // console.dir(list.style.visibilty);
+//     // console.log(list.style.visibilty);
+    
+//     if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty") {
+//         alert("All fields required.");
+//     } else if (validateInput(pilot) === "Ia a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number" || validateInput(cargoLevel) === "Not a Number") {
+//         alert("Please enter valid information.")
+//     } else {
+
+//         list.style.visibility = "visible";
+//         pilotStatus.innerHTML = `Pilot ${pilot} is ready`;
+//         copilotStatus.innerHTML = `Pilot ${copilot} is ready`;
+//         if (fuelLevel)
+//         } else if (fuelLevel < 10000) {
+//             fuelStatus.innerHTML = "Not enough fuel for journey";
+//             launchStatus.innerHTML = "Shuttle not ready for launch";
+//             launchStatus.style.color = "rgb(199, 37, 78)";
+//         } else if (cargoLevel > 10000) {
+//             cargoStatus.innerHTML = "Cargo Mass is too high for journey";
+//             launchStatus.innerHTML = "Shuttle not ready for launch";
+//             launchStatus.style.color = "rgb(199, 37, 78)";
+//         } else if (fuelLevel >= 10000 && caroLevel <= 10000) {
+//             launchStatus.innerHTML = "Shuttle is ready for launch";
+//             launchStatus.style.color = "rgb(65, 159, 106)";
+//         }
+//     }
+    
+    
    
 }
 
